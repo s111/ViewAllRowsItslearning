6c415cdae934142a05d35cd7110e5e77fa341985
Sebastian M. Pedersen <sem.pedersen@stud.uis.no>
2016-03-01 13:17:06 +0100

    Added support for Grade book and Assignment report

--- ViewAllRowsItslearning.user.js
+++ ViewAllRowsItslearning.user.js
@@ -3,26 +3,37 @@
 // @namespace   https://github.com/s111
 // @description Adds an option to view all rows when correcting assigments on itslearning
 // @include     https://*.itslearning.com/essay/read_essay.aspx?EssayID=*
+// @include     https://*.itslearning.com/AssessmentRecord/Grades.aspx?CourseID=*
+// @include     https://*.itslearning.com/AssessmentRecord/Assignments.aspx?CourseID=*
 // @version     1
 // @grant       none
 // ==/UserScript==
 
 // String on the form '[start] to [end] of [maxSize]'.
-var selectionInfo = $('.page-size-chooser span');
+var sizeInfo = $('.page-size-chooser span');
 
-// Selector for the page size drop-down.
-var select = $('#EssayAnswers_PageSizeSelect');
+// Selectors for the page size drop-downs.
+var pageSelect = $('#EssayAnswers_PageSizeSelect');
+var gridSelect = $('#ctl00_ContentPlaceHolder_GridPager_PagerDropdown');
 
-if (selectionInfo.length && select.length) {
-  var info = selectionInfo.text().split(" ");
+if (sizeInfo.length) {
+  if (pageSelect.length) {
+    appendOption(pageSelect, sizeInfo);
+  } else if (gridSelect.length) {
+    appendOption(gridSelect, sizeInfo);
+  }
+}
+
+function appendOption(selectElement, sizeInfo) {
+  var info = sizeInfo.text().split(" ");
 
   var start = info[0];
   var end = info[2];
   var maxSize = info[4];
 
-  select.append('<option value="' + maxSize + '">View all</option>');
+  selectElement.append('<option value="' + maxSize + '">View all</option>');
 
   // Set the correct value in the drop-down
   var currentSize = end - start + 1;
-  select.val(currentSize);
+  selectElement.val(currentSize);
 }
